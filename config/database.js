04514607b9205be23029fc02b41b79efb818e8c6
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-09-16 16:14:28 +0200

    chore: adding mongo atlas connection

--- config/database.js
+++ config/database.js
@@ -1,19 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'mongo'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        uri: process.env.DB_URI,
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
   },
